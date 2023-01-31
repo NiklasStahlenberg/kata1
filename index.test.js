46c1f97c8c1336c466ca18cb9f5acfeef47b41c9
Simon Palmberg <simon.palmberg@gmail.com>
2023-01-31 10:19:57 +0100

    Fizzbuzz

--- index.test.js
+++ index.test.js
@@ -1,11 +1,8 @@
-function sum(a, b) {
-  return a + b;
-}
-
 function fizzBuzz(arr) {
-  const sorted = arr.sort((a, b) => b - a);
+  const sorted = arr.sort((a, b) => a - b);
   const replaced = sorted.map((num) => {
-    if (num % 3 === 0 && num % 5) {
+    if (num === 0) return num;
+    if (num % 3 === 0 && num % 5 === 0) {
       return "FizzBuzz";
     } else if (num % 3 === 0) {
       return "fizz";
@@ -14,14 +11,11 @@ function fizzBuzz(arr) {
     }
     return num;
   });
+  console.log(replaced);
+  return replaced;
 }
-
-test("adds 1 + 2 to equal 3", () => {
-  expect(sum(1, 2)).toBe(3);
-});
-
 test("number that can be divided by three should be replaced with fizz", () => {
   let arr = [0, 1, 2, 3, 4, 5, 6];
 
-  expect(fizzBuzz(arr).toBe([0, 1, 2, "fizz", 4, 5, "fizz"]));
+  expect(fizzBuzz(arr)).toBe([0, 1, 2, "fizz", 4, "buzz", "fizz"]);
 });
