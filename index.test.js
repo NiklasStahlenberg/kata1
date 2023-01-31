8da96e229fcffb06320f114603e855ff3d5d465c
Simon Palmberg <simon.palmberg@gmail.com>
2023-01-31 10:13:56 +0100

    Started fizzBuzz

--- index.test.js
+++ index.test.js
@@ -2,6 +2,14 @@ function sum(a, b) {
   return a + b;
 }
 
+function fizzBuzz(arr) {
+  const sorted = arr.sort((a, b) => b - a);
+  const replaced = sorted.map((num) => {
+    if (num % 3 === 0 && num % 5) {
+    }
+  });
+}
+
 test("adds 1 + 2 to equal 3", () => {
   expect(sum(1, 2)).toBe(3);
 });
