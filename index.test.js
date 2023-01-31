61bc21503c863fdf69f06fc16212720884e5c135
niklasstahlenberg <niklas@sekurbit.se>
2023-01-31 10:13:58 +0100

    unit test

--- index.test.js
+++ index.test.js
@@ -5,3 +5,9 @@ function sum(a, b) {
 test("adds 1 + 2 to equal 3", () => {
   expect(sum(1, 2)).toBe(3);
 });
+
+test("number that can be divided by three should be replaced with fizz", () => {
+  let arr = [0, 1 ,2 , 3, 4, 5, 6];
+
+   expect(fizzBuzz(arr).toBe([0, 1, 2, "fizz", 4, 5, "fizz"]));
+})
